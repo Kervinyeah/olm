262880fd4d0288d80faf2f3caabccb910b0ebbf9
zzp <zhouzhaoping11@gmail.com>
2014-10-06 15:22:44 +0800

    test deletecookie

---
+++ deletecookie.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php 
+setcookie("user", NULL);
+echo "delete<br />";
+?>
+
+</body>
+</html>
--- writecookie.php
+++ writecookie.php
@@ -1,9 +1,16 @@
-<?php 
-setcookie("user", "Alex Porter", time()+3600);
-?>
-
 <html>
 <body>
-<p>hehe</p>
+
+<?php 
+if (isset($_COOKIE["user"]))
+	echo "old<br />";
+else
+{
+	setcookie("user", "Alex Porter", time()+3600*24*20);
+	echo "new<br />";
+}
+echo "Welcome " . $_COOKIE["user"] . "!<br />";
+?>
+
 </body>
 </html>
